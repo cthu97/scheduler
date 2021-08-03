0a2192f0c98009c0c8fa8c079d4b273116813717
Cat <catherinehu_9898@hotmail.com>
2021-08-03 16:54:16 -0700

    w07d2 done, added appointment and schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,11 +1,53 @@
 import React, { useState } from "react";
-
-import DayList from 'components/DayList';
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
 
-
 export default function Application(props) {
+  const appointments = [
+    {
+      id: 1,
+      time: "12pm",
+    },
+    {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 1,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        },
+      },
+    },
+    {
+      id: 3,
+      time: "3pm",
+      interview: {
+        student: "Ranger Joe of Kentucky Fried Mushrooms",
+        interviewer: {
+          id: 1,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        },
+      },
+    },
+    {
+      id: 4,
+      time: "4pm",
+      interview: {
+        student: "Unicorn Lily From The Land of Kazeebas",
+        interviewer: {
+          id: 1,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        },
+      },
+    },
+  ];
+
   const [day, setDay] = useState("Monday");
 
   const days = [
@@ -25,36 +67,30 @@ export default function Application(props) {
       spots: 0,
     },
   ];
-  
+
   return (
     <main className="layout">
       <section className="sidebar">
-        <img className="sidebar--centered"
+        <img
+          className="sidebar--centered"
           src="images/logo.png"
           alt="Interview Scheduler"
         />
-        <DayList
-          days={days}
-          day={day}
-          setDay={setDay}
-        />
-
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-
-
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
       </section>
-
-
-
     </main>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -149,4 +150,37 @@ storiesOf("DayList", module)
       />
     ))
     .add("Status", () => (<Status message="Deleting" />))
-    .add("Error", () => (<Error message="Could not delete appointment." onClose={action("onClose")}/>))
+    .add("Error", () => (<Error 
+      message="Could not delete appointment." 
+      onClose={action("onClose")}/>
+      ))
+      .add("Form Edit", () => (<Form 
+      name="Dave" 
+      interviewers={interviewers} 
+      interviewer={2} 
+      onSave={action("onSave")} 
+      onCancel={action("onCancel")} />
+      ))
+      .add("Form Create", () => (<Form 
+      interviewers={interviewers} 
+      onSave={action("onSave")} 
+      onCancel={action("onCancel")} />
+      ))
+
+      .add("Appointment Empty", () => (
+        <Fragment>
+          <Appointment id={1} time="12pm" />
+          <Appointment id="last" time="1pm" />
+        </Fragment>
+      ))
+      .add("Appointment Booked", () => (
+        <Fragment>
+          <Appointment
+            id={1}
+            time="12pm"
+            interview={{ student: "Lydia Miller-Jones", interviewer }}
+          />
+          <Appointment id="last" time="1pm" />
+        </Fragment>
+      ))
+      
